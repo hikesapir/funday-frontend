10267b6e63c1420a0fece9bbf31cdbbbd0ffce1f
lioramar55 <lioramar55@gmail.com>
2022-03-22 18:47:26 +0200

    started adding relevant components

---
+++ src/components/board-group.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="board-group">
+    <task-preview></task-preview>
+  </section>
+</template>
+
+<script>
+import taskPreview from './task-preview.vue'
+export default {
+  name: 'board-group',
+  components: {
+    taskPreview,
+  },
+}
+</script>
