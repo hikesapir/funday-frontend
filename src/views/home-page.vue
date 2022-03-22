38a2c0dab20eaf5105464f71df8acdc2ce713f9a
lioramar55 <lioramar55@gmail.com>
2022-03-22 18:27:08 +0200

    I'm in

--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -1,10 +1,7 @@
-<script setup>
-// import TheWelcome from '@/components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <!-- <TheWelcome /> -->
     <h1>Welcome funday</h1>
   </main>
 </template>
+
+<script></script>
