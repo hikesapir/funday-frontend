c8d801c84894c0786ca59f1f47f92295999c138e
Roee Furman <roeefurman1@gmail.com>
2022-03-22 18:10:54 +0200

    app structure

---
+++ monday-json.js
@@ -0,0 +1,269 @@
+// Guidelines
+// boardStore (no need for groupStore, taskStore), boardService
+// *. Support saving the entire board and also on the task level, 
+// PUT /api/board/b123/task/t678
+//    (no need for seperate APIs for mini-updates of task parts like members or status)
+
+// *. No need for saving an activities array per task, those activities are easily filtered from the board activities
+// *. activites - when board is updated, the frontend does not send the activities array within the board 
+//    instead it only sends a new activity object: {txt, boardId, groupId, taskId}
+//    the backend adds this activity to the board with $push and can also emit socket notificatios
+// *. D & D Guidelines - vue-smooth-dnd / vuedraggable / react-beutiful-dnd
+// *. Same model for Monday style app (do not implement a generic columns feature)
+// *. We do not handle concurrent editing - needs versioning
+
+// Rendering performance:
+// Store Mutation - saveBoard
+// state.board = board
+// Later, support switching a specific task
+
+
+// <BoardDetails> => <BoardGroup v-for>
+// <BoardGroup> => <TaskPreview v-for>
+// <TaskDetails> (+edit) - initially can be loaded in seperate route (later on we can place it in a modal and nested route)
+
+
+
+// Store - saveTask
+function storeSaveTask(task, activity) {
+    const activity = {
+        "id": makeId(),
+        "txt": "Changed Color",
+        "createdAt": Date.now(),
+        "byMember": userService.getLoggedinUser(),
+        "task": task
+    }
+    board = boardService.saveTask(boardId, groupId, task, activity)
+    commit(board)
+}
+
+// boardService
+function saveTask(boardId, groupId, task, activity) {
+    const board = getById(boardId)
+    // TODO: find the task, and update
+    board.activities.unshift(activity)
+    saveBoard(board)
+    return board
+}
+
+// boardStore-action
+async function loadAndWatchBoard(boardId) {
+    // load from service and commit to store
+    // subscribe to socket and commit to store
+}
+
+
+
+const board = {
+    "_id": "b101", //mongoDB
+    "title": "Robot dev proj", //user input
+    "createdAt": 1589983468418, // date.now auto (service)
+    "createdBy": {
+        "_id": "u101", //make ID
+        "fullname": "Abi Abambi", //miniuser
+        "imgUrl": "http://some-img" //miniuser
+    },
+    "style": {}, //board view: charts / groups / table / canvan-trello
+    "labels": [ //starred or add new status for example
+        {
+            "id": "l101",
+            "title": "Done",
+            "color": "#61bd4f"
+        },
+        {
+            "id": "l102",
+            "title": "Progress",
+            "color": "#61bd33"
+        }
+    ],
+    "members": [ //board members
+        {
+            "_id": "u101",
+            "fullname": "Tal Tarablus",
+            "imgUrl": "https://www.google.com"
+        }
+    ],
+    "groups": [ //tasks
+        {
+            "id": "g101",
+            "title": "Group 1",
+            "tasks": [
+                {
+                    "id": "c101",
+                    "title": "Replace logo"
+                },
+                {
+                    "id": "c102",
+                    "title": "Add Samples"
+                }
+            ],
+            "style": {}
+        },
+        {
+            "id": "g102",
+            "title": "Group 2",
+            "tasks": [
+                {
+                    "id": "c103",
+                    "title": "Do that"
+                },
+                {
+                    "id": "c104",
+                    "title": "make it happen",
+                    "status": "in-progress", //dropdown
+                    "priority": '', //dropdown
+                    "timeline": '', //time
+                    // "description": "description",
+                    "tags": '',
+                    "files": '',
+                    "updates": [ //updates
+                        {
+                            "id": "ZdPnm", //makeID
+                            "txt": "also @yaronb please CR this",
+                            "createdAt": 1590999817436, //date.now
+                            "byMember": {
+                                "_id": "u101", //makeId
+                                "fullname": "Tal Tarablus",
+                                "imgUrl": "http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg"
+                            }
+                        }
+                    ],
+                    "members": [
+                        {
+                            "_id": "u101",
+                            "username": "Tal",
+                            "fullname": "Tal Tarablus",
+                            "imgUrl": "http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg"
+                        }
+                    ],
+                    "labelIds": ["l101", "l102"],
+                    "createdAt": 1590999730348,
+                    "dueDate": 16156215211,
+                    "byMember": { //static data
+                        "_id": "u101",
+                        "username": "Tal",
+                        "fullname": "Tal Tarablus",
+                        "imgUrl": "http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg"
+                    },
+                    // "style": {
+                    //     "bgColor": "#26de81" 
+                    // }
+                }
+            ],
+            "style": {} //color per group picked by user / auto
+        }
+    ],
+    "activities": [ //per board not other collection
+        {
+            "id": "a101",
+            "txt": "Changed Color",
+            "createdAt": 154514,
+            "byMember": {
+                "_id": "u101",
+                "fullname": "Abi Abambi",
+                "imgUrl": "http://some-img"
+            },
+            // "type": "task" / "board" / "group"
+            "description": {
+                "id": "c101",
+                "type": "task", //todo: maybe add group
+                "activity": "Replace Logo"
+            }
+            // "board":{
+
+            // }
+        }
+    ],
+    // for monday
+    "cmpsOrder":
+        ["title-picker", "members-picker", "priority-picker", "status-picker", "timeline-picker", "tags-picker", "files-picker"]
+}
+const user = {
+    "_id": "u101",
+    "fullname": "Abi Abambi",
+    "username": "abi@ababmi.com",
+    "password": "aBambi123",
+    "imgUrl": "http://some-img.jpg",
+    "mentions": [{
+        "id": "m101",
+        "boardId": "m101",
+        "taskId": "t101"
+    }]
+}
+
+// For Monday Mostly:
+// Dynamic Components: 
+// <TaskPreview> => <tr> 
+//    <td v-for="(cmpType) in cmpsOrder">
+//         <component :is="cmpType" :info="getCmpInfo(cmpType)" @updated="updateTask(cmpType, $event)">
+//    </td>
+
+function updateTask(cmpType, data) {
+    // Switch
+    // task.members = data;
+    // task.status = data;
+}
+
+
+const cmp1 = {
+    type: 'status-picker',
+    info: {
+        selectedStatus: 'pending',
+        statuses: [{}, {}]
+    }
+}
+
+const cmp2 = {
+    type: 'member-picker',
+    info: {
+        selectedMembers: ['m1', 'm2'],
+        members: ['m1', 'm2', 'm3']
+    }
+}
+
+const cmp3 = {
+    type: 'date-picker',
+    info: {
+        selectedDate: '2022-09-07',
+    }
+}
+
+
+// export function TaskPreview({ task }) {
+//     //GET FROM STORE
+//     const cmpsOrder = [
+//       "status-picker",
+//       "member-picker",
+//       "date-picker",
+//       "priority-picker",
+//     ];
+//     return (
+//       <section>
+//         <h5>{task.txt}</h5>
+//         {cmpsOrder.map((cmp, idx) => {
+//           return (
+//             <DynamicCmp
+//               cmp={cmp}
+//               key={idx}
+//               onUpdate={(data) => {
+//                 console.log("Updating: ", cmp, "with data:", data);
+//                 // make a copy, update the task
+//                 // Call action: updateTask(task)
+//               }}
+//             />
+//           );
+//         })}
+//       </section>
+//     );
+//   }
+
+// export function DynamicCmp({ cmp, info, onUpdate }) {
+//     switch (cmp) {
+//         case "status-picker":
+//             return <StatusCmp info={info} onUpdate={onUpdate} />;
+//         case "member-picker":
+//             return <MemberPicker info={info} onUpdate={onUpdate} />;
+//         default:
+//             return <p>UNKNOWN {cmp}</p>;
+//     }
+// }
---
+++ src/views/home-page.vue
@@ -0,0 +1,10 @@
+<script setup>
+// import TheWelcome from '@/components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <h1>Welcome funday</h1>
+  </main>
+</template>
