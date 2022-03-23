8340b91235ba6cb3d2afd0ea1e440c532f8f7f46
Roee Furman <roeefurman1@gmail.com>
2022-03-23 12:51:53 +0200

    tags

--- src/components/dynamic/tag-picker.vue
+++ src/components/dynamic/tag-picker.vue
@@ -1,7 +1,28 @@
-<template></template>
+<template>
+  <div>
+    <div v-for="tag in task.tags" :key="tag" class="tag-picker-val">
+      #{{ tag }}
+    </div>
+  </div>
+</template>
 
 <script>
 export default {
-  name: 'tag-picker',
-}
+  name: "tag-picker",
+  props: {
+    task: Object,
+  },
+  data() {
+    return {
+      tags: this.$store.getters.board,
+    };
+  },
+  computed: {
+    style() {
+      console.log(this.tags);
+      // console.log(this.statuses);
+      // return this.statuses.find((status) => status.id === this.task.status);
+    },
+  },
+};
 </script>
