5fcaf1eac9e49d8f50253f66b35489fafd1b69c4
Roee Furman <roeefurman1@gmail.com>
2022-03-23 11:30:32 +0200

    status cmp

--- src/components/dynamic/status-picker.vue
+++ src/components/dynamic/status-picker.vue
@@ -1,7 +1,17 @@
-<template></template>
+<template>
+  <div>
+    <div class="status-picker-col" style="background-color: black">
+      {{ task.status }}
+    </div>
+  </div>
+</template>
 
 <script>
 export default {
-  name: 'status-picker',
-}
+  name: "status-picker",
+  props: {
+    task: Object,
+  },
+  name: "status-picker",
+};
 </script>
