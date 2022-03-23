f21b29634b0435cde24a256d05b32db64f446447
sapir hiki <sapirhiki@gmail.com>
2022-03-24 01:55:05 +0200

    fixed some

--- src/components/context-modal.vue
+++ src/components/context-modal.vue
@@ -1,42 +1,53 @@
 <template>
-  <section class="context-modal">
-    <button @click="openNewTab">
-      Open Board in New Tab
-    </button>
-    <button @click="renameBoard">Rename Board</button>
-    <button @click="staerred">Add to favorites</button>
-    <button>Duplicate Board</button>
-    <button @click="remove">Delete</button>
-  </section>
+    <section class="context-modal">
+        <button @click="openNewTab">Open Board in New Tab</button>
+        <button @click="renameBoard">Rename Board</button>
+        <button @click="starred">Add to favorites</button>
+        <button @click="duplicate">Duplicate Board</button>
+        <button @click="remove">Delete</button>
+    </section>
 </template>
 
+
 <script>
+
 export default {
-  name: 'context-modal',
-  props: {},
-  emits: ['openNewTab', 'remove'],
-  components: {},
-  data() {
-    return {}
-  },
-  created() {},
-  mounted() {},
-  methods: {
-    remove() {
-      this.$emit('remove')
+    name: 'context-modal',
+    props: {
     },
-    openNewTab() {
-      this.$emit('openNewTab')
+    emits: ['openNewTab', 'remove', 'starred', 'renameBoard', 'duplicate'],
+    components: {},
+    data() {
+        return {
+        }
     },
-    renameBoard() {
-      this.$emit('renameBoard')
+    created() {
+
     },
-    starred() {
-      console.log('star')
-      this.$emit('starred')
+    mounted() {
     },
-  },
-  computed: {},
-  unmounted() {},
+    methods: {
+        remove() {
+            this.$emit('remove')
+        },
+        openNewTab() {
+            this.$emit('openNewTab')
+
+        },
+        renameBoard() {
+            this.$emit('renameBoard')
+        },
+        starred() {
+            this.$emit('starred')
+        },
+        duplicate() {
+            this.$emit('duplicate')
+        }
+    },
+    computed: {
+
+    },
+    unmounted() {
+    },
 }
 </script>
