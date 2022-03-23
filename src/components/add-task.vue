4679ac1cde613a9df94cc10e7daaeea99337dd03
lioramar55 <lioramar55@gmail.com>
2022-03-23 17:52:25 +0200

    styled the add-task component

--- src/components/add-task.vue
+++ src/components/add-task.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="add-task">
+    <div
+      class="left-indicator-inner"
+      :style="leftBoxStyle"
+    ></div>
     <form @submit.prevent="onSubmit">
       <input
         type="text"
+        @focus="toggleFocues"
+        @blur="toggleFocues"
         v-model="task.title"
         placeholder="+ Add task"
       />
-      <button>Add</button>
+      <button :class="btnClass">Add</button>
     </form>
   </div>
 </template>
@@ -16,9 +22,13 @@ import boardService from '../services/board-service.js'
 
 export default {
   name: 'add-task',
+  props: {
+    groupId: String,
+  },
   data() {
     return {
       task: boardService.getEmptyTask(),
+      isFocused: false,
     }
   },
   methods: {
@@ -28,6 +38,25 @@ export default {
         JSON.parse(JSON.stringify(this.task))
       )
       this.task = boardService.getEmptyTask()
+    },
+    toggleFocues() {
+      this.isFocused = !this.isFocused
+    },
+  },
+  computed: {
+    leftBoxStyle() {
+      const group = this.$store.getters.board?.groups.find(
+        (group) => group.id === this.groupId
+      )
+      console.log('group?.style.color', group?.style.color)
+      return {
+        'background-color': group?.style.color,
+        opacity: 0.6,
+      }
+    },
+    btnClass() {
+      console.log('this.isFocused', this.isFocused)
+      return this.isFocused ? 'focus' : ''
     },
   },
 }
