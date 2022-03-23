561f016e483cf19cb50f8d201f58df441e041458
sapir hiki <sapirhiki@gmail.com>
2022-03-23 20:00:33 +0200

    option to remove board

--- src/components/board-preview.vue
+++ src/components/board-preview.vue
@@ -1,26 +1,31 @@
 <template>
-    <li class="board-preview" @mouseover="mouseOver"  @mouseleave="mouseLeve" >
+    <li v-if="board" class="board-preview" @mouseover="mouseOver" @mouseleave="mouseLeve">
         <div class="bord-title" @click="selectBoard(board._id)">
             <span>{{ board.title }}</span>
-            <button v-if="isHover">
+            <button @click="openModal = !openModal" v-if="isHover || openModal">
                 <i class="fa-solid fa-ellipsis"></i>
             </button>
         </div>
     </li>
+    <context-modal v-if="openModal" @remove="remove" />
 </template>
 
 
 <script>
+import contextModal from "./context-modal.vue"
 export default {
     name: 'board-preview',
     props: {
         board: Object
     },
     emits: [],
-    components: {},
+    components: {
+        contextModal,
+    },
     data() {
         return {
             isHover: false,
+            openModal: false,
         }
     },
     created() {
@@ -35,6 +40,12 @@ export default {
         mouseLeve() {
             this.isHover = false
         },
+        remove() {
+            // this.$emit('remove', this.board._id)
+            this.$store.dispatch({ type: 'removeBoard', boardId: this.board._id})
+
+            // console.log(this.board._id);
+        }
 
     },
     computed: {
---
+++ src/components/context-modal.vue
@@ -0,0 +1,40 @@
+<template>
+   <section class="context-modal">
+       <button>Open Board in New Tab</button>
+       <button>Rename Board</button>
+       <button>Add to favorites</button>
+       <button>Duplicate Board</button>
+       <button @click="remove">Delete</button>
+   </section>
+</template>
+
+
+<script>
+
+export default {
+    name: 'context-modal',
+    props: {
+    },
+    emits: [,'remove'],
+    components: {},
+    data() {
+        return {
+        }
+    },
+    created() {
+
+    },
+    mounted() {
+    },
+    methods: {
+        remove(){
+            this.$emit('remove')
+        }
+    },
+    computed: {
+
+    },
+    unmounted() {
+    },
+}
+</script>
