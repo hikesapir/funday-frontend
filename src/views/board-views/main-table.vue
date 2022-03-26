b77ec44ca2265080e4414ffa044378a8a7db9eaf
sapir hiki <sapirhiki@gmail.com>
2022-03-26 16:24:46 +0300

    better handling

--- src/views/board-views/main-table.vue
+++ src/views/board-views/main-table.vue
@@ -1,21 +1,7 @@
 <template>
-<<<<<<< HEAD
   <div v-if="board" class="main-table">
-    <Container v-if="board?.groups" @drop="onDrop" drag-handle-selector=".drag-handle">
+    <Container v-if="board?.groups" @drop="onDrop" orientation="vertical" drag-handle-selector=".drag-handle">
       <Draggable v-for="group in board?.groups" :key="group.id">
-=======
-  <div class="main-table">
-    <Container
-      v-if="board?.groups"
-      @drop="onDrop"
-      orientation="vertical"
-      drag-handle-selector=".drag-handle"
-    >
-      <Draggable
-        v-for="group in board?.groups"
-        :key="group.id"
-      >
->>>>>>> a59c44c65d4d23db062b7d7e3c815af2fa1b95a0
         <board-group
           :group="JSON.parse(JSON.stringify(group))"
           :cmpsOrder="board?.cmpsOrder"
