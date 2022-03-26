1ad10f897c7aa8e3fa42f430e3f56ab69ca57b7f
Roee Furman <roeefurman1@gmail.com>
2022-03-26 19:36:54 +0300

    status

--- src/components/sum-preview.vue
+++ src/components/sum-preview.vue
@@ -38,23 +38,11 @@ export default {
   methods: {},
   computed: {
     cmpsOrder() {
-<<<<<<< HEAD
-      console.log("example", this.$store.getters.board?.cmpsOrder);
       const cmps = JSON.parse(
         JSON.stringify(this.$store.getters.board?.cmpsOrder)
       );
       cmps.shift();
       return cmps.map((cmp) => cmp.cmpName.replace("picker", "sum"));
-      // console.log('cmps', cmps)
-=======
-      const cmps = JSON.parse(
-        JSON.stringify(this.$store.getters.board?.cmpsOrder)
-      )
-      cmps.shift()
-      return cmps.map((cmp) =>
-        cmp.cmpName.replace('picker', 'sum')
-      )
->>>>>>> fd45729ade8a54e7587cc217519d8772eae15572
     },
     boardData() {
       const { boardMapByGroups } = this.$store.getters.boardData;
