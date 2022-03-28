225ab329ac8429e88d5cb75047c19b985d26761b
lioramar55 <lioramar55@gmail.com>
2022-03-28 13:31:29 +0300

    added a modal to pick a tag for a task or add new tag

--- src/components/dynamic/tag-picker.vue
+++ src/components/dynamic/tag-picker.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="tag-picker">
-    <div @click="isModalOpen = true" class="add-tag">
+  <section class="tag-picker">
+    <div @click="toggleModal" class="add-tag">
       <fa icon="circle-plus" />
     </div>
     <div
@@ -11,7 +11,32 @@
     >
       #{{ tag.txt }}
     </div>
-  </div>
+    <div v-if="isModalOpen" class="tag-modal">
+      <div class="add-tags">
+        <input
+          type="text"
+          placeholder="Add tags"
+          v-model="newTag"
+        />
+      </div>
+      <ul class="tag-search-list">
+        <li
+          v-for="tag in groupTags"
+          :key="tag.txt"
+          class="tag-search-item"
+          @mouseover="toggleHoverTag(tag.txt)"
+          @mouseleave="toggleHoverTag()"
+          :style="{
+            color:
+              isHoverATag === tag.txt ? 'white' : tag.color,
+          }"
+        >
+          {{ tag.txt }}
+        </li>
+      </ul>
+      <button class="btn">+ Create new tag</button>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -19,13 +44,39 @@ export default {
   name: 'tag-picker',
   props: {
     task: Object,
+    groupId: String,
   },
   data() {
     return {
       tags: this.$store.getters.board,
       isModalOpen: false,
+      isHoverATag: false,
+      newTag: '',
     }
   },
-  methods: {},
+  computed: {
+    groupTags() {
+      let groups = JSON.parse(
+        JSON.stringify(
+          this.$store.getters.boardData.boardMapByGroups
+        )
+      )
+      const groupTags = groups.reduce((acc, group) => {
+        if (group.id === this.groupId) {
+          acc.push(...group.tags)
+        }
+        return acc
+      }, [])
+      return groupTags
+    },
+  },
+  methods: {
+    toggleHoverTag(tag = null) {
+      this.isHoverATag = tag
+    },
+    toggleModal() {
+      this.isModalOpen = !this.isModalOpen
+    },
+  },
 }
 </script>
