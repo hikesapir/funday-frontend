a4de37b938ae6825939c2e5bcb1f0be01b4c50e2
sapir hiki <sapirhiki@gmail.com>
2022-04-01 22:14:28 +0300

    file preview

---
+++ src/components/dynamic/preview/file-preview.vue
@@ -0,0 +1,40 @@
+<template>
+    <span class="file-preview">
+        <img
+            @mouseover="isHover = true"
+            @mouseleave="isHover = false"
+            :src="file"
+            alt
+            @click="openNewTab(file)"
+        />
+        <div v-if="isHover" class="context-modal">
+            <img :src="file" alt />
+        </div>
+    </span>
+</template>
+
+
+<script>
+export default {
+    name: 'file-preview',
+    props: { file: String },
+    emits: [],
+    components: {},
+    data() {
+        return {
+            isHover: false,
+
+        }
+    },
+    created() {
+
+    },
+    mounted() {
+    },
+    methods: {},
+    computed: {
+    },
+    unmounted() {
+    },
+}
+</script>
