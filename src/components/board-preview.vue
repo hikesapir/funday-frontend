b80d56ccf089c22e3274041f642d6068a4c09fce
sapir hiki <sapirhiki@gmail.com>
2022-04-01 11:08:13 +0300

    fixed the bord nav

--- src/components/board-preview.vue
+++ src/components/board-preview.vue
@@ -1,5 +1,11 @@
 <template>
-  <li v-if="board" class="board-preview" @mouseover="isHover = true" @mouseleave="isHover = false">
+  <li
+    v-if="board"
+    class="board-preview"
+    @mouseover="isHover = true"
+    @mouseleave="isHover = false"
+    :class="active"
+  >
     <label v-if="changeName">
       <input
         @keyup.enter="updateBoard"
@@ -11,7 +17,27 @@
       />
     </label>
     <div v-else class="bord-title" @click="selectBoard">
-      <span>{{ board.title }}</span>
+      <div class="space-btn">
+        <span class="align-center">
+          <svg
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            width="19"
+            height="19"
+            aria-hidden="true"
+            aria-label="Public board"
+            class="icon_component"
+          >
+            <path
+              d="M7.5 4.5H16C16.2761 4.5 16.5 4.72386 16.5 5V15C16.5 15.2761 16.2761 15.5 16 15.5H7.5L7.5 4.5ZM6 4.5H4C3.72386 4.5 3.5 4.72386 3.5 5V15C3.5 15.2761 3.72386 15.5 4 15.5H6L6 4.5ZM2 5C2 3.89543 2.89543 3 4 3H16C17.1046 3 18 3.89543 18 5V15C18 16.1046 17.1046 17 16 17H4C2.89543 17 2 16.1046 2 15V5Z"
+              fill="currentColor"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </span>
+        <div class="ellipsis">{{ board.title }}</div>
+      </div>
       <button @click.stop="openModal = !openModal" v-if="isHover || openModal">
         <i class="fa-solid fa-ellipsis"></i>
       </button>
@@ -115,7 +141,19 @@ export default {
       document.title = this.$store.getters.boards[idx].title
     },
   },
-  computed: {},
+  computed: {
+    active() {
+      return (this.$route.params.id === this.board._id) ? 'active' : ''
+    }
+  },
   unmounted() { },
 }
 </script>
+
+<style>
+.active {
+  background-color: #cce5ff;
+  border-radius: 4px;
+  border: 1px solid transparent;
+}
+</style>
